2dc13f477700ca537d4588ca42084e7286641b47
prasunmatrix <prasun@matrixnmedia.com>
2023-02-28 19:11:55 +0530

    payment blade interface change

--- resources/views/stripepayment/stripe.blade_bkp28feb2023.php
+++ resources/views/stripepayment/stripe.blade_bkp28feb2023.php
@@ -23,7 +23,7 @@
                      </div>
                      @endif
                      <br>
-                     <!-- <form role="form" action="{{ route('stripe.post') }}" method="post" class="require-validation" data-cc-on-file="false" data-stripe-publishable-key="{{ env('STRIPE_KEY') }}" id="payment-form">
+                     <form role="form" action="{{ route('stripe.post') }}" method="post" class="require-validation" data-cc-on-file="false" data-stripe-publishable-key="{{ env('STRIPE_KEY') }}" id="payment-form">
                         @csrf
                         <div class='form-row row'>
                            <div class='col-xs-12 col-md-6 form-group required'>
@@ -54,30 +54,7 @@
                               <button class="btn btn-primary btn-lg btn-block" type="submit">Pay Now</button>
                            </div>
                         </div>
-                     </form> -->
-                     <div class="card">
-                <form action="{{ route('stripe.post') }}" method="post" id="payment-form">
-                    @csrf                    
-                    <div class="form-group">
-                        <div class="card-header">
-                            <label for="card-element">
-                                Enter your credit card information
-                            </label>
-                        </div>
-                        <div class="card-body">
-                            <div id="card-element">
-                            <!-- A Stripe Element will be inserted here. -->
-                            </div>
-                            <!-- Used to display form errors. -->
-                            <div id="card-errors" role="alert"></div>
-                            <input type="hidden" name="plan" value="{{-- $plan->id --}}" />
-                        </div>
-                    </div>
-                    <div class="card-footer">
-                        <button class="btn btn-dark" type="submit">Pay</button>
-                    </div>
-                </form>
-            </div>
+                     </form>
                   </div>
                </div>
             </div>
@@ -85,8 +62,8 @@
       </div>
    </body>   
 </html>
-<script type="text/javascript" src="https://js.stripe.com/v3/"></script>
-<!-- <script type="text/javascript">
+<script type="text/javascript" src="https://js.stripe.com/v2/"></script>
+<script type="text/javascript">
 $(function() {
     var $form = $(".require-validation");
     $('form.require-validation').bind('submit', function(e) {
@@ -132,77 +109,4 @@ $(function() {
         }
     }
 });
-</script> -->
-<script>
-    // Create a Stripe client.
-var stripe = Stripe('{{ env("STRIPE_KEY") }}');
-
-// Create an instance of Elements.
-var elements = stripe.elements();
-
-// Custom styling can be passed to options when creating an Element.
-// (Note that this demo uses a wider set of styles than the guide below.)
-var style = {
-  base: {
-    color: '#32325d',
-    lineHeight: '18px',
-    fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
-    fontSmoothing: 'antialiased',
-    fontSize: '16px',
-    '::placeholder': {
-      color: '#aab7c4'
-    }
-  },
-  invalid: {
-    color: '#fa755a',
-    iconColor: '#fa755a'
-  }
-};
-
-// Create an instance of the card Element.
-var card = elements.create('card', {style: style});
-
-// Add an instance of the card Element into the `card-element` <div>.
-card.mount('#card-element');
-
-// Handle real-time validation errors from the card Element.
-card.addEventListener('change', function(event) {
-  var displayError = document.getElementById('card-errors');
-  if (event.error) {
-    displayError.textContent = event.error.message;
-  } else {
-    displayError.textContent = '';
-  }
-});
-
-// Handle form submission.
-var form = document.getElementById('payment-form');
-form.addEventListener('submit', function(event) {
-  event.preventDefault();
-
-  stripe.createToken(card).then(function(result) {
-    if (result.error) {
-      // Inform the user if there was an error.
-      var errorElement = document.getElementById('card-errors');
-      errorElement.textContent = result.error.message;
-    } else {
-      // Send the token to your server.
-      stripeTokenHandler(result.token);
-    }
-  });
-});
-
-// Submit the form with the token ID.
-function stripeTokenHandler(token) {
-  // Insert the token ID into the form so it gets submitted to the server
-  var form = document.getElementById('payment-form');
-  var hiddenInput = document.createElement('input');
-  hiddenInput.setAttribute('type', 'hidden');
-  hiddenInput.setAttribute('name', 'stripeToken');
-  hiddenInput.setAttribute('value', token.id);
-  form.appendChild(hiddenInput);
-
-  // Submit the form
-  form.submit();
-}
 </script>
