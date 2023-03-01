df0ac4d9fff6d8cdddf66a8870c3d1d11d97669f
prasunmatrix <prasun@matrixnmedia.com>
2023-03-01 17:24:20 +0530

    remove zip code from form

--- resources/views/stripepayment/stripe.blade.php
+++ resources/views/stripepayment/stripe.blade.php
@@ -160,7 +160,7 @@ var style = {
 };
 
 // Create an instance of the card Element.
-var card = elements.create('card', {style: style});
+var card = elements.create('card', {hidePostalCode: true,style: style});
 
 // Add an instance of the card Element into the `card-element` <div>.
 card.mount('#card-element');
